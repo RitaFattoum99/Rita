1ac80f1136f3b3096600276607c8ed33a8f144d0
RitaFattoum99 <ritafattoum99@gmail.com>
2021-10-18 18:40:40 +0300

    Add BMI folder

---
+++ BMI/main.js
@@ -0,0 +1,36 @@
+    //  Start Calculate BMI 
+    function calc() {
+        let height = document.querySelector("#height").value;
+        let weight = document.querySelector("#weight").value;
+        let result = document.querySelector("#result");
+        let img = document.createElement("img");
+        result.value = ((weight * 10000) / (height * height)).toFixed(2);
+        document.querySelector("#result").innerHTML = Math.round(result.value);
+
+        // Underweight
+        if (result.value <= 18.5) {
+            document.querySelector("#Presult").innerHTML = "Underweight";
+            img.setAttribute("src", "under.png");
+            document.querySelector("#imageBMI").appendChild(img);
+        }
+        //  Normal
+        else if (result.value >= 18.5 && result.value <= 24.9) {
+            document.querySelector("#Presult").innerHTML = "Normal";
+            img.setAttribute("src", "normal.png");
+            document.querySelector("#imageBMI").appendChild(img);
+        }
+        // Overweight
+        else if (result.value >= 25 && result.value <= 29.9) {
+            document.querySelector("#Presult").innerHTML = "Overweight";
+            img.setAttribute("src", "obes1.png");
+            document.querySelector("#imageBMI").appendChild(img);
+        }
+        // Obese
+        else {
+            document.querySelector("#Presult").innerHTML = "Obese";
+            img.setAttribute("src", "obes2.png");
+            document.querySelector("#imageBMI").appendChild(img);
+        }
+    };
+    document.querySelector("#click").onclick = calc;
+    //  End Calculate BMI 
