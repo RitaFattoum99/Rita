10e79695822775d93cb76d5f360834a7fa75c2e4
RitaFattoum99 <ritafattoum99@gmail.com>
2021-10-19 13:01:42 +0300

    add file

---
+++ python/classes/class.py
@@ -0,0 +1,23 @@
+class Point ():
+    def __init__(self , input1 , input2):
+        self.x = input1
+        self.y = input2
+
+p = Point(8 , 2)
+
+print(p.x)
+print(p.y)
+
+
+# ///////////////////////////////////////////////
+
+class Flight ():
+    def __init__(self , capacity):
+        self capacity = capacity
+        self passengers = []
+
+def add_passengers (self ,name):
+    if 
+def open_seats (self):
+    return self.capacity - len(self.passengers)
+
