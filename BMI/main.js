42ed08910bb08df1ac907183045a029bc0de4446
RitaFattoum99 <ritafattoum99@gmail.com>
2021-10-22 18:05:35 +0300

    correct the path of photo

--- BMI/main.js
+++ BMI/main.js
@@ -10,25 +10,25 @@
         // Underweight
         if (result.value <= 18.5) {
             document.querySelector("#Presult").innerHTML = "Underweight";
-            img.setAttribute("src", "under.png");
+            img.setAttribute("src", "/photo/under.png");
             document.querySelector("#imageBMI").appendChild(img);
         }
         //  Normal
         else if (result.value >= 18.5 && result.value <= 24.9) {
             document.querySelector("#Presult").innerHTML = "Normal";
-            img.setAttribute("src", "normal.png");
+            img.setAttribute("src", "/photo/normal.png");
             document.querySelector("#imageBMI").appendChild(img);
         }
         // Overweight
         else if (result.value >= 25 && result.value <= 29.9) {
             document.querySelector("#Presult").innerHTML = "Overweight";
-            img.setAttribute("src", "obes1.png");
+            img.setAttribute("src", "/photo/obes1.png");
             document.querySelector("#imageBMI").appendChild(img);
         }
         // Obese
         else {
             document.querySelector("#Presult").innerHTML = "Obese";
-            img.setAttribute("src", "obes2.png");
+            img.setAttribute("src", "/photo/obes2.png");
             document.querySelector("#imageBMI").appendChild(img);
         }
     };
