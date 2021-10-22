deea569f7a8443e67271346afcbdfaf059225ff4
RitaFattoum99 <ritafattoum99@gmail.com>
2021-10-23 00:18:49 +0300

    add first project\

---
+++ website/google/views.py
@@ -0,0 +1,21 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+import datetime
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello World!")
+
+# def greet(request ,name):
+#     return HttpResponse(f"Hello, {name}!".title())
+
+# def greet(request, name):
+#     return render (request, 'hello/index.html', {
+#         "name" : name.title()
+#     })
+
+def christmas(request):
+    now = datetime.datetime.now()
+    return render(request, 'hello/index.html', {
+        "christmas" : now.month == 1 and now.day == 1 
+    })
