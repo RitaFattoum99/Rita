bbf4a7d10f86ef530204240be147ecc7fc9ce1b7
RitaFattoum99 <ritafattoum99@gmail.com>
2021-10-23 23:38:19 +0300

    add calculate age in django..

--- website/google/urls.py
+++ website/google/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns =[
     path("", views.index, name="index"),
-    path("<str:name>", views.greet, name="greet"),
+    # path("<str:name>", views.greet, name="greet"),
     path("<int:birth>", views.birth, name="birth"),
     # path("christmas",views.christmas, name="christmas")
 ]
