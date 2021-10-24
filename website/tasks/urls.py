dc1bc04f6fb505df9a201616c510f9cce7d1a2c3
RitaFattoum99 <ritafattoum99@gmail.com>
2021-10-24 18:59:47 +0300

    update folders with error

--- website/tasks/urls.py
+++ website/tasks/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns =[
     path("", views.index, name="index"),
+    path("tasks/", views.add, name="add")
 ]
--- website/tasks/views.py
+++ website/tasks/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
+from django import forms
 import datetime
 # Create your views here.
 
 app_name = "tasks"
+
+tasks = ["Check Email", "Make Files", "Run As Administartor"]
 
 def index(request):
     now = datetime.datetime.now()
     return render (request, "tasks/index.html",{
         "now" : now
     })
+
+def add(request):
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="Add Task")
